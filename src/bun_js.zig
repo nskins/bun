aec0a0f31519a209f461ff3ef7a9e37c410b8788
Derrick Farris <mr.dcfarris@gmail.com>
2023-02-11 20:55:47 -0600

    wip(tty): cache if fd is tty, need to fix how header is shared

--- src/bun_js.zig
+++ src/bun_js.zig
@@ -48,6 +48,8 @@ pub const Run = struct {
         JSC.markBinding(@src());
         @import("bun.js/javascript_core_c_api.zig").JSCInitialize();
 
+        _ = C.setTtyOrigTermiosIfNeeded();
+
         js_ast.Expr.Data.Store.create(default_allocator);
         js_ast.Stmt.Data.Store.create(default_allocator);
         var arena = try Arena.init();
@@ -220,6 +222,8 @@ pub const Run = struct {
         vm.global.handleRejectedPromises();
 
         vm.onExit();
+
+        _ = C.setTtyRawMode(false);
 
         if (!JSC.is_bindgen) JSC.napi.fixDeadCodeElimination();
         Global.exit(@boolToInt(this.any_unhandled));
